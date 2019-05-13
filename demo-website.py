ec73936e2a14c01627a11d7e5ab4b008bd089789
tbaums <michael.a.tanenbaum@gmail.com>
2019-05-13 15:43:01 -0400

    first try of build pipeline

--- demo-website.py
+++ demo-website.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 @app.route('/where')
 def where():
     hostname = socket.gethostname()
-    return f'Website running at host {hostname}\n'
+    return f'\n Version 0.1 \n Website running at host {hostname}\n'
 
 @app.route('/health')
 def health():
